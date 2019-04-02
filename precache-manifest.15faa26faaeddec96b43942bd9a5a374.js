b937864171c6ad87b7bea28a2fb6478c26fa2d9b
Supreme Salvatore <milosposkuricakg@gmail.com>
2019-04-02 18:21:11 +0200

    Updates

---
+++ precache-manifest.15faa26faaeddec96b43942bd9a5a374.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6793df0a35d1b9dbd8f",
+    "url": "/react-recipe-finder/static/js/runtime~main.a6793df0.js"
+  },
+  {
+    "revision": "5f45398f1545b4c3c56b",
+    "url": "/react-recipe-finder/static/js/main.5f45398f.chunk.js"
+  },
+  {
+    "revision": "8c644281963b69121a1d",
+    "url": "/react-recipe-finder/static/js/2.8c644281.chunk.js"
+  },
+  {
+    "revision": "5f45398f1545b4c3c56b",
+    "url": "/react-recipe-finder/static/css/main.82c0b6c1.chunk.css"
+  },
+  {
+    "revision": "078405bf127ec25356dfae1fa350568d",
+    "url": "/react-recipe-finder/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-recipe-finder/precache-manifest.15faa26faaeddec96b43942bd9a5a374.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-recipe-finder/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
